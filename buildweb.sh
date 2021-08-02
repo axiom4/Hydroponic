5400f808b7dcea99dfcfbdf3b369a53edadc73c4
Riccardo Giannetto <riccardo.giannetto@par-tec.it>
2021-08-02 09:05:14 +0200

    update buildweb

--- buildweb.sh
+++ buildweb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd HydroponicWeb
-npm install && ng build --output-hashing=none --aot --optimization --output-path=../data --delete-output-path && gzip -9 ../data/*
+git pull && npm install && ng build --output-hashing=none --aot --optimization --output-path=../data --delete-output-path && gzip -9 ../data/*
